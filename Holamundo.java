0a0ed666d2db08e23148a32d0697bea118eb12e9
Raul-Armijos <raarmijos15@utpl.edu.ec>
2025-10-22 12:35:17 -0500

    hola mundo profesor daniel irene

--- Holamundo.java
+++ Holamundo.java
@@ -3,6 +3,6 @@ public class Holamundo {
     
     public static void main(String[] args) {
 
-        System.out.println("¡Hola, Mundo!");
+        System.out.println("¡Hola, Mundo Profesor Daniel Irene");
     }
  }
